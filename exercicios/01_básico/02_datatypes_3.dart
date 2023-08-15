85a41a6ea80b590073c609002b67831811c33737
mjpcarvalho <a18493@alunos.ipb.pt>
2023-08-15 03:06:51 +0100

    15:08:2023-03:06

---
+++ exercicios/01_básico/02_datatypes_3.dart
@@ -0,0 +1,22 @@
+main() {
+  // String
+
+  String nome_usuario = "João";
+  String sobrenome_usuario = 'Carvalho';
+  print(nome_usuario); // João
+  print(sobrenome_usuario); // Carvalho
+
+  // Upercase
+
+  nome_usuario = nome_usuario.toUpperCase();
+  print(nome_usuario);
+  sobrenome_usuario = sobrenome_usuario.toUpperCase();
+  print(sobrenome_usuario);
+
+  // Lowercase
+
+  nome_usuario = nome_usuario.toLowerCase();
+  print(nome_usuario);
+  sobrenome_usuario = sobrenome_usuario.toLowerCase();
+  print(sobrenome_usuario);
+}
