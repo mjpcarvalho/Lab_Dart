f9770eebfb739e88434f748611218b832673a051
João Carvalho <mjp.carvalho@gmail.com>
2023-08-17 00:46:50 +0100

    17:08:2023-00:46

--- exercicios/01_básico/02_datatypes_4.dart
+++ exercicios/01_básico/02_datatypes_4.dart
@@ -1,6 +1,40 @@
 main() {
 // String
 
-  String nome = 'tony';
-  print(nome);
+  String nome = 'João';
+  String nome2 = "João";
+
+// String - A questão das aspas e verificar se estas equivalem
+  String nome3 = 'O\'Connor';
+  String nome4 = "O'Connor";
+
+  print(nome2 == nome2);
+  print(nome3);
+  print(nome4);
+  print(nome3 == nome4);
+
+// String - Multilinha
+
+  String multilinha = '''
+Olá mundo
+Como estás?
+''';
+
+  print(multilinha);
+
+// String - Multilinha
+  String multilinha2 = '''
+Olá mundo
+$nome
+Como estás?
+''';
+
+  print(multilinha2);
+
+// String - Otra forma de representar um nome com duas variáveis na mesma linha
+
+  String Apelido = "Carvalho";
+  String nomecompleto = "$nome $Apelido";
+
+  print(nomecompleto);
 }
