88a27de79547872181ad11bfc24d6483a4a5edd8
mjpcarvalho <a18493@alunos.ipb.pt>
2023-08-17 19:23:23 +0100

    17:08:2023-19:22

---
+++ exercicios/01_básico/02_datatypes_5.dart
@@ -0,0 +1,12 @@
+main() {
+// Boleanos
+
+  bool isActive = true;
+
+  // Negação de isActive
+  bool isNotActive = !isActive;
+
+  print(isActive);
+
+  print(isNotActive);
+}
---
+++ exercicios/01_básico/02_datatypes_6.dart
@@ -0,0 +1,14 @@
+main() {
+// Listas
+  List<String> listacervejas = ["Sagres", "Super Bock", "Cristal"];
+
+  print(listacervejas);
+
+  // Substituição de um valor
+
+  List<String> listacervejas2 = ["Sagres", "Super Bock", "Cristal"];
+
+  listacervejas2[2] = "Guinness";
+
+  print(listacervejas2);
+}
