d37a207326e8901d63f249e7ab93d44dcda4726f
mjpcarvalho <a18493@alunos.ipb.pt>
2023-08-19 02:58:47 +0100

    19:08:2023-02:58

---
+++ exercicios/01_básico/02_datatypes_8.dart
@@ -0,0 +1,26 @@
+main() {
+// 02-12
+// maps
+
+// Mapa do tipo: String / Dinamic
+  Map<String, dynamic> superbock = {
+    "cor": "Loira",
+    "poder": "Dá tração às quatro!",
+    "nivel": 9000,
+  };
+
+  // Imprimir a chave e o valor
+  print(superbock);
+
+  // Como imprimir só um valor ou mais...
+  print(superbock["poder"]);
+
+  // Mapa do tipo: String / Dinamic
+  Map<int, dynamic> sagres = {
+    1: "Loira",
+    2: "Dá tracçãon às quatro!",
+    3: 9000,
+  };
+  // Imprimir a chave e o valor
+  print(sagres);
+}
