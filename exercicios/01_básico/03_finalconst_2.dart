0896699a3da29be0a916b57479e9c0c66c162061
mjpcarvalho <a18493@alunos.ipb.pt>
2023-08-29 15:35:44 +0100

    29:08:2023-15:35

---
+++ exercicios/01_básico/03_finalconst_2.dart
@@ -0,0 +1,17 @@
+main() {
+// 03-04
+// late
+
+  int a;
+  final double b = 10;
+  const double c = 20;
+
+  final double z;
+
+  double x = 10.25;
+
+  c.add("carro");
+
+  print(x);
+  print(c);
+}
